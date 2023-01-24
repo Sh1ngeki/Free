a344d803650551d8005a8e427e699d325999b3aa
nika <nika2005nika2005@gmail.com>
2023-01-24 16:21:44 +0400

    rotation

--- app/src/main/java/com/example/jiy/MainActivity.kt
+++ app/src/main/java/com/example/jiy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jiy
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
 
 
