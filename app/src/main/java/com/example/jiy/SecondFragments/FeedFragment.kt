85d7a4143ad7ab71adaacf82c6d4dd9692f2a6d5
nika <nika2005nika2005@gmail.com>
2023-01-24 14:49:53 +0400

    viewpager

--- app/src/main/java/com/example/jiy/SecondFragments/FeedFragment.kt
+++ app/src/main/java/com/example/jiy/SecondFragments/FeedFragment.kt
@@ -7,27 +7,34 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-import com.example.jiy.Loading.ViewPagerFragmentAdapter
 import com.example.jiy.R
+import com.example.jiy.viewpager.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class FeedFragment:Fragment(R.layout.feed_fragment) {
-    private lateinit var viewPager2: ViewPager2
+class FeedFragment : Fragment() {
+
+    private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
-    private lateinit var viewPagerFragmentAdapter: ViewPagerFragmentAdapter
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.feed_fragment, container, false)
-        viewPager2 = view.findViewById(R.id.view_pager)
+
+        viewPager = view.findViewById(R.id.view_pager)
         tabLayout = view.findViewById(R.id.tabLayout)
-        viewPagerFragmentAdapter = ViewPagerFragmentAdapter(this.requireContext() as AppCompatActivity)
-        TabLayoutMediator(tabLayout, viewPager2){
-                tab,position ->
-            when(position){
-                0 -> tab.text = "Friends"
-                1 -> tab.text = "Discovery"
-            }
-        }
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = adapter.getPageTitle(position)
+
+        }.attach()
+
         return view
     }
+
 }
