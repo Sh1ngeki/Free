03ef051cb5eb9437f41649bdf1d3e4e3e9475303
nika <nika2005nika2005@gmail.com>
2023-01-25 23:22:58 +0400

    wava ra1

---
+++ app/src/main/java/com/example/jiy/postistvis/Commenting.kt
@@ -0,0 +1,30 @@
+package com.example.jiy.postistvis
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.jiy.R
+import com.example.jiy.viewpagerpages.SecondPage
+
+class Commenting:Fragment(R.layout.comment_layout) {
+
+
+    private lateinit var commentText: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view =inflater.inflate(R.layout.comment_layout,container,false)
+        println("haimeeeee")
+        commentText=view.findViewById(R.id.comment)
+        println(SecondPage.commentdata.commentText)
+        commentText.setText(SecondPage.commentdata.commentText)
+
+
+
+
+        return view
+    }
+}
